09fded9e7ab747c63fba0a8de62dec28c4c14183
fjl716 <fjl716@163.com>
2018-06-26 11:45:12 +0800

    将sink替换成mint

--- mq_message.py
+++ mq_message.py
@@ -1,5 +1,5 @@
 from protobuf3.message import Message
-from protobuf3.fields import BytesField, MessageField, Int64Field, StringField
+from protobuf3.fields import StringField, BytesField, Int32Field, Int64Field, MessageField
 
 
 class Address(Message):
@@ -15,10 +15,9 @@ class MQMessage(Message):
 
 Address.add_field('matrix', StringField(field_number=1, optional=True))
 Address.add_field('device', StringField(field_number=2, optional=True))
-MintPayload.add_field('matrix', StringField(field_number=1, optional=True))
-MintPayload.add_field('angler', StringField(field_number=2, optional=True))
-MintPayload.add_field('number', StringField(field_number=3, optional=True))
-MintPayload.add_field('time', Int64Field(field_number=4, optional=True))
+MintPayload.add_field('partition', Int32Field(field_number=1, optional=True))
+MintPayload.add_field('number', StringField(field_number=2, optional=True))
+MintPayload.add_field('time', Int64Field(field_number=3, optional=True))
 MQMessage.add_field('source', MessageField(field_number=1, optional=True, message_cls=Address))
 MQMessage.add_field('destination', MessageField(field_number=2, optional=True, message_cls=Address))
 MQMessage.add_field('resource', StringField(field_number=3, optional=True))
--- resources/mint.py
+++ resources/mint.py
@@ -5,8 +5,7 @@ from angler.mq_message import MintPayload
 class MintMQ(MQHandler):
     def heart(self):
         payload = MintPayload()
-        payload.matrix = self.angler.matrix
-        payload.angler = self.angler.name
+        payload.partition = self.payload[0]
         payload.number = self.angler.number
         payload.time = self.time
         data = payload.encode_to_bytes()
